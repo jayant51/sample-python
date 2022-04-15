ef2649a27c1a77f1df568b87d5bd81592c23c439
jayantkulkarni <jayant.kulkarni@ibm.com>
2022-04-14 23:54:26 -0700

    Added API

--- app.py
+++ app.py
@@ -16,5 +16,34 @@ def restartVM():
     return "Message : Sent Restart remote server : ***REMOVED***!"  
 
 
+#-----------------------------------------------------------------------------------------------------------------------
+# Utils class 
+# getSSHClient : Obtains SSHClient to execute command over SSH
+# execCommand : function to execute command over SSH, which also closes connection after command execution
+#-----------------------------------------------------------------------------------------------------------------------
+class utils():
+	def getSSHClient():
+		sshclnt = paramiko.SSHClient()
+		sshclnt.set_missing_host_key_policy(paramiko.AutoAddPolicy())   
+		#sshclnt.connect("***REMOVED***", port=****, username=******, password=********)
+		#sshclnt.connect("***REMOVED***", port=****, username=******, password=============)
+		sshclnt.connect("***REMOVED***", port=*****, username=******, password=============)
+		return sshclnt
+
+	def execCommand(command):
+		try:
+			sshclnt = utils.getSSHClient()
+			stdin, stdout, stderr = sshclnt.exec_command(command)
+			print ("stdin", file=sys.stdin)
+			print ("stdout", file=sys.stdout)
+			print ("stderr=", file=sys.stderr)
+			opt = stdout.readlines()
+			opt = "".join(opt)
+			print(opt)
+		except Exception as ex:
+			print("Authentication failed, please verify your credentials: %s" % ex)
+		finally:
+			sshclnt.close() 
+
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=8080)
